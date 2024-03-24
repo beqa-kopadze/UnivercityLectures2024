f3d17da77ec4d6b4fe01593ec68b744dcf55b5f9
beqa-kopadze <beqa.tyupebi@gmail.com>
2024-03-24 18:56:40 +0400

    changed getting messages from packages

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import p1.C1;
+import p1.C2;
+import p1.p11.C11;
+import p2.C3;
+
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(C1.MESSAGE);
+        System.out.println(C2.MESSAGE);
+        System.out.println(C11.MESSAGE);
+        System.out.println(C3.MESSAGE);
     }
 }
--- src/p1/C1.java
+++ src/p1/C1.java
@@ -5,12 +5,4 @@ import p2.C3;
 
 public class C1 {
     public static String MESSAGE = "This is C1 message";
-
-    public static void main(String[] args) {
-        System.out.println(C1.MESSAGE);
-        System.out.println(C2.MESSAGE);
-        System.out.println(C11.MESSAGE);
-        System.out.println(C3.MESSAGE);
-
-    }
 }
